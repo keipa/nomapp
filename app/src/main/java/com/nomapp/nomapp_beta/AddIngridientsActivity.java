9b30adac9bae912dffafc4f09b3308cc21764104
antonid08 <antonenkoilya01@gmail.com>
2015-07-05 11:34:59 +0300

    Simple database and icon has been added.

---
+++ app/src/main/java/com/nomapp/nomapp_beta/AddIngridientsActivity.java
@@ -0,0 +1,21 @@
+package com.nomapp.nomapp_beta;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by antonid on 05.07.2015.
+ */
+
+public class AddIngridientsActivity extends Activity {
+    StartActivity main = new StartActivity();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_ingridients);
+
+    }
+
+
+}
--- app/src/main/java/com/nomapp/nomapp_beta/StartActivity.java
+++ app/src/main/java/com/nomapp/nomapp_beta/StartActivity.java
@@ -1,17 +1,46 @@
 package com.nomapp.nomapp_beta;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
-public class StartActivity extends ActionBarActivity {
+public class StartActivity extends ActionBarActivity implements View.OnClickListener {
+
+    final String LOG_TAG = "myLogs";
+
+    Button addIngridients;
+
+    public String[] ingridients  = {"Svekla", "Morkov"};
+
+    DBHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        dbHelper = new DBHelper(this);
+
+        addIngridients = (Button) findViewById(R.id.addIngBtn);
+        addIngridients.setOnClickListener(this);
+
+        ContentValues cv = new ContentValues();
+
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        cv.put("name", ingridients[0]);
+        //long rowID = db.insert("mytable", null, cv);
+        //Log.d(LOG_TAG, "row inserted, ID = " + rowID);
     }
 
     @Override
@@ -35,4 +64,42 @@ public class StartActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.addIngBtn:
+                Intent intent = new Intent(StartActivity.this, AddIngridientsActivity.class);
+                startActivity(intent);
+                break;
+
+            default:
+
+                break;
+        }
+    }
+
+    class DBHelper extends SQLiteOpenHelper {
+
+        public DBHelper(Context context) {
+            // конструктор суперкласса
+            super(context, "myDB", null, 1);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            Log.d(LOG_TAG, "--- onCreate database ---");
+            // создаем таблицу с полями
+            db.execSQL("create table mytable ("
+                    + "id integer primary key autoincrement,"
+                    + "name text,"
+                    + ");");
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        }
+    }
+
 }
