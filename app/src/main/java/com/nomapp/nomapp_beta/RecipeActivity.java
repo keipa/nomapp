74524a3390d23283674f26e36e5f28e59c0fb227
antonid08 <antonenkoilya01@gmail.com>
2015-07-31 22:38:03 +0300

    new activity lol.

---
+++ app/src/main/java/com/nomapp/nomapp_beta/RecipeActivity.java
@@ -0,0 +1,15 @@
+package com.nomapp.nomapp_beta;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by antonid on 31.07.2015.
+ */
+public class RecipeActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipes);
+    }
+}
