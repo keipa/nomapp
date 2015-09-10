9dc58cf0a7cd861c85ce521bfb417d0bf6454f6c
antonid08 <antonenkoilya01@gmail.com>
2015-09-11 00:41:48 +0300

    gridView
    Adapter and onClickListener for gridView added.

--- app/src/main/java/com/nomapp/nomapp_beta/Activities/CategoriesActivity.java
+++ app/src/main/java/com/nomapp/nomapp_beta/Activities/CategoriesActivity.java
@@ -3,53 +3,60 @@ package com.nomapp.nomapp_beta.Activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.GridView;
 
+import com.nomapp.nomapp_beta.CategoriesGVAdapter;
 import com.nomapp.nomapp_beta.R;
 
 /**
  * Created by antonid on 20.08.2015.
  */
 public class CategoriesActivity extends Activity implements View.OnClickListener {
+    Button btnSearch;
 
-
-
-    String[] data = {"Мясо", "Птица", "Рыба", "Морепродукты", "Овощи", "Фрукты и ягоды", "Бакалея", "Крупы", "Молочные продукты", "Грибы", "Зелень", "Орехи", "Готовые продукты"};
-    Integer[] nums = {1,2,3,4,5,6,7,8,9,10,11,12,13};
-    GridView gvMain;
+   // Integer[] nums = {1,2,3,4,5,6,7,8,9,10,11,12,13};
+    GridView categoriesGridView;
     ArrayAdapter<String> adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
+
         Window window = getWindow();
         window.setStatusBarColor(getResources().getColor(R.color.colorMainDark));
-        setContentView(R.layout.activity_categories_grid);
-        adapter = new ArrayAdapter<String>(this, R.layout.category_item, R.id.textView5, data);
-        gvMain = (GridView) findViewById(R.id.gridView);
-        gvMain.setAdapter(adapter);
+
+        btnSearch = (Button) findViewById(R.id.btnSearch);
+        btnSearch.setOnClickListener(this);
+
+        //adapter = new ArrayAdapter<String>(this, R.layout.category_item, R.id.textView5, data);
+        //adapter = new CategoriesGVAdapter(this);
+        categoriesGridView = (GridView) findViewById(R.id.gridView);
+        categoriesGridView.setAdapter(new CategoriesGVAdapter(this));
         adjustGridView();
+        categoriesGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Log.w("MY_TAG","gridItem clicked");
+                Intent toIngs = new Intent (CategoriesActivity.this, AddIngridientsActivity.class);
+                toIngs.putExtra("numberOfCategory", position + 1);
+                startActivity(toIngs);
+            }
+        });
 
     }
 
-
-    private void adjustGridView(){
-        gvMain.setNumColumns(2);
-        gvMain.setHorizontalSpacing(5);
-        gvMain.setVerticalSpacing(5);
-    }
     @Override
     protected void onStart() {
         super.onStart();
-
-
     }
 
     @Override
@@ -75,6 +82,18 @@ public class CategoriesActivity extends Activity implements View.OnClickListener
 
     @Override
     public void onClick(View v) {
-
+        switch (v.getId()){
+            case R.id.btnSearch:
+                Intent intent = new Intent(CategoriesActivity.this, SearchIngredientsActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
+
+    private void adjustGridView(){
+        categoriesGridView.setNumColumns(2);
+        categoriesGridView.setHorizontalSpacing(5);
+        categoriesGridView.setVerticalSpacing(5);
+    }
+
 }
