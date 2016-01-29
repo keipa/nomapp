cc79a1c229380181eba799908c7ea214dd6f8fdd
Antonenko Ilya <antonenkoilya01@gmail.com>
2016-01-29 15:27:20 +0300

    FlexibleSpaceWithImage recycler added.
    Need to set up getData();

--- app/src/main/java/com/nomapp/nomapp_beta/AddIngredients/AddIngridientsActivity.java
+++ app/src/main/java/com/nomapp/nomapp_beta/AddIngredients/AddIngridientsActivity.java
@@ -1,22 +1,36 @@
 package com.nomapp.nomapp_beta.AddIngredients;
 
+import android.annotation.TargetApi;
 import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.TypedArray;
 import android.database.Cursor;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import com.github.ksoichiro.android.observablescrollview.ObservableRecyclerView;
+import com.github.ksoichiro.android.observablescrollview.ObservableScrollViewCallbacks;
+import com.github.ksoichiro.android.observablescrollview.ScrollState;
+import com.github.ksoichiro.android.observablescrollview.ScrollUtils;
 import com.melnykov.fab.FloatingActionButton;
+import com.nineoldandroids.view.ViewHelper;
 import com.nomapp.nomapp_beta.AllRecipes.AllRecipesActivity;
 import com.nomapp.nomapp_beta.CategoriesOfRecipes.CategoriesOfRecipesActivity;
 import com.nomapp.nomapp_beta.Database.Database;
@@ -24,12 +38,14 @@ import com.nomapp.nomapp_beta.NavigationDrawer.NavDrawerListAdapter;
 import com.nomapp.nomapp_beta.R;
 import com.nomapp.nomapp_beta.Start.StartActivity;
 
+import java.util.ArrayList;
+
 
 /**
  * Created by antonid on 05.07.2015.
  */
 
-public class AddIngridientsActivity extends android.support.v7.app.AppCompatActivity {
+public class AddIngridientsActivity extends android.support.v7.app.AppCompatActivity implements ObservableScrollViewCallbacks{
     ActionBarDrawerToggle mDrawerToggle;
     ListView mDrawerList;
     DrawerLayout mDrawerLayout;
@@ -38,17 +54,73 @@ public class AddIngridientsActivity extends android.support.v7.app.AppCompatActi
 
     FloatingActionButton fab;
 
+    private static final int NUM_OF_ITEMS = 100;
+
+    private static final float MAX_TEXT_SCALE_DELTA = 0.3f;
+
+    private View mImageView;
+    private View mOverlayView;
+    private View mRecyclerViewBackground;
+    private TextView mTitleView;
+    private int mActionBarSize;
+    private int mFlexibleSpaceImageHeight;
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_ingredients);
 
-        AddIngredientsRecyclerFragment frag = new AddIngredientsRecyclerFragment();
-        FragmentTransaction trans = getFragmentManager().beginTransaction();
-        trans.add(R.id.add_ings_fragment_cont,frag);
-        trans.commit();
 
-        Window window = getWindow();
+        mFlexibleSpaceImageHeight = getResources().getDimensionPixelSize(R.dimen.flexible_space_image_height);
+        mActionBarSize = getActionBarSize();
+
+        ObservableRecyclerView recyclerView = (ObservableRecyclerView) findViewById(R.id.recycler);
+        recyclerView.setScrollViewCallbacks(this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(false);
+        final View headerView = LayoutInflater.from(this).inflate(R.layout.recycler_header, null);
+        headerView.post(new Runnable() {
+            @Override
+            public void run() {
+                headerView.getLayoutParams().height = mFlexibleSpaceImageHeight;
+            }
+        });
+        setDummyDataWithHeader(recyclerView, headerView);
+
+        mImageView = findViewById(R.id.image);
+        mOverlayView = findViewById(R.id.overlay);
+
+        mTitleView = (TextView) findViewById(R.id.title);
+        mTitleView.setText(getTitle());
+        setTitle(null);
+
+        // mRecyclerViewBackground makes RecyclerView's background except header view.
+        mRecyclerViewBackground = findViewById(R.id.list_background);
+
+        //since you cannot programmatically add a header view to a RecyclerView we added an empty view as the header
+        // in the adapter and then are shifting the views OnCreateView to compensate
+        final float scale = 1 + MAX_TEXT_SCALE_DELTA;
+        mRecyclerViewBackground.post(new Runnable() {
+            @Override
+            public void run() {
+                ViewHelper.setTranslationY(mRecyclerViewBackground, mFlexibleSpaceImageHeight);
+            }
+        });
+        ViewHelper.setTranslationY(mOverlayView, mFlexibleSpaceImageHeight);
+        mTitleView.post(new Runnable() {
+            @Override
+            public void run() {
+                ViewHelper.setTranslationY(mTitleView, (int) (mFlexibleSpaceImageHeight - mTitleView.getHeight() * scale));
+                ViewHelper.setPivotX(mTitleView, 0);
+                ViewHelper.setPivotY(mTitleView, 0);
+                ViewHelper.setScaleX(mTitleView, scale);
+                ViewHelper.setScaleY(mTitleView, scale);
+            }
+        });
+
+
+   //     Window window = getWindow();
 //        window.setStatusBarColor(getResources().getColor(R.color.notification));
         fab = (FloatingActionButton) findViewById(R.id.fabtohome);  //floating action button init
         setUpFAB();
@@ -78,8 +150,86 @@ public class AddIngridientsActivity extends android.support.v7.app.AppCompatActi
     }
 
 
+    @Override
+    public void onScrollChanged(int scrollY, boolean firstScroll, boolean dragging) {
+        // Translate overlay and image
+        float flexibleRange = mFlexibleSpaceImageHeight - mActionBarSize;
+        int minOverlayTransitionY = mActionBarSize - mOverlayView.getHeight();
+        ViewHelper.setTranslationY(mOverlayView, ScrollUtils.getFloat(-scrollY, minOverlayTransitionY, 0));
+        ViewHelper.setTranslationY(mImageView, ScrollUtils.getFloat(-scrollY / 2, minOverlayTransitionY, 0));
+
+        // Translate list background
+        ViewHelper.setTranslationY(mRecyclerViewBackground, Math.max(0, -scrollY + mFlexibleSpaceImageHeight));
+
+        // Change alpha of overlay
+        ViewHelper.setAlpha(mOverlayView, ScrollUtils.getFloat((float) scrollY / flexibleRange, 0, 1));
+
+        // Scale title text
+        float scale = 1 + ScrollUtils.getFloat((flexibleRange - scrollY) / flexibleRange, 0, MAX_TEXT_SCALE_DELTA);
+        setPivotXToTitle();
+        ViewHelper.setPivotY(mTitleView, 0);
+        ViewHelper.setScaleX(mTitleView, scale);
+        ViewHelper.setScaleY(mTitleView, scale);
+
+        // Translate title text
+        int maxTitleTranslationY = (int) (mFlexibleSpaceImageHeight - mTitleView.getHeight() * scale);
+        int titleTranslationY = maxTitleTranslationY - scrollY;
+        ViewHelper.setTranslationY(mTitleView, titleTranslationY);
+    }
+
+    @Override
+    public void onDownMotionEvent() {
+    }
+
+    @Override
+    public void onUpOrCancelMotionEvent(ScrollState scrollState) {
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    private void setPivotXToTitle() {
+        Configuration config = getResources().getConfiguration();
+        if (Build.VERSION_CODES.JELLY_BEAN_MR1 <= Build.VERSION.SDK_INT
+                && config.getLayoutDirection() == View.LAYOUT_DIRECTION_RTL) {
+            ViewHelper.setPivotX(mTitleView, findViewById(android.R.id.content).getWidth());
+        } else {
+            ViewHelper.setPivotX(mTitleView, 0);
+        }
+    }
+
+
+
+    protected int getActionBarSize() {
+        TypedValue typedValue = new TypedValue();
+        int[] textSizeAttr = new int[]{R.attr.actionBarSize};
+        int indexOfAttrTextSize = 0;
+        TypedArray a = obtainStyledAttributes(typedValue.data, textSizeAttr);
+        int actionBarSize = a.getDimensionPixelSize(indexOfAttrTextSize, -1);
+        a.recycle();
+        return actionBarSize;
+    }
+
+
+    public static ArrayList<String> getDummyData() {
+        return getDummyData(NUM_OF_ITEMS);
+    }
+
+    public static ArrayList<String> getDummyData(int num) {
+        ArrayList<String> items = new ArrayList<>();
+        for (int i = 1; i <= num; i++) {
+            items.add("Item " + i);
+        }
+        return items;
+    }
+
+    protected void setDummyDataWithHeader(RecyclerView recyclerView, View headerView) {
+        recyclerView.setAdapter(new SimpleHeaderRecyclerAdapter(this, getDummyData(), headerView));
+    }
+
+
+
+
     void setUpNavigationDraver() {
-        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+      /*  mToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
 
         actionBar = getSupportActionBar();
@@ -109,7 +259,7 @@ public class AddIngridientsActivity extends android.support.v7.app.AppCompatActi
         String title = categoryCursor.getString(1);
         actionBar.setTitle(title);
         categoryCursor.close();
-
+*/
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerLayout.setStatusBarBackgroundColor(getResources().getColor(R.color.notification));
         mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.app_name, R.string.app_name);
@@ -125,6 +275,9 @@ public class AddIngridientsActivity extends android.support.v7.app.AppCompatActi
 
 
     }
+
+
+
 
     private class DrawerItemClickListener implements ListView.OnItemClickListener {
         @Override
